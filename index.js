f8bbdd70aebc77f40e3d86b0b5aa353b9b09d6be
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2020-03-07 14:14:24 +0700

    add moving marker

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ app.get('/', (req, res) => {
 app.get('/index.html',(req, res)=>{
   res.sendFile(__dirname + '/index.html');
 });
-app.get('/map.html',(req, res)=>{
-  res.sendFile(__dirname + '/map.html');
-});
-app.get('/detail.html',(req, res)=>{
-  res.sendFile(__dirname + '/detail.html');
-});
-app.get('/manual.html',(req, res)=>{
-  res.sendFile(__dirname + '/manual.html');
-});
-app.get('/setting.html',(req, res)=>{
-  res.sendFile(__dirname + '/setting.html');
-});
+// app.get('/map.html',(req, res)=>{
+//   res.sendFile(__dirname + '/map.html');
+// });
+// app.get('/detail.html',(req, res)=>{
+//   res.sendFile(__dirname + '/detail.html');
+// });
+// app.get('/manual.html',(req, res)=>{
+//   res.sendFile(__dirname + '/manual.html');
+// });
+// app.get('/setting.html',(req, res)=>{
+//   res.sendFile(__dirname + '/setting.html');
+// });
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.post('/manual.html', (req,res)=>{
 //   // console.log(req.body.fname,req.body.lname)
@@ -44,12 +44,6 @@ app.post('/', (req,res)=>{
   }
 })
 
-<<<<<<< HEAD
-// static files
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('public'));
-=======
->>>>>>> 71a5617b9f20b71c920dae60e5a449a352e27f8e
 app.use(express.static(__dirname + '/public'));
 
 var SerialPort = require('serialport');
@@ -63,19 +57,8 @@ const parserATS = new parsers.Readline({
   delimiter: '\r\n'
 })
 
-<<<<<<< HEAD
-// var port = new SerialPort('', {
-//   baudRate: 38400 
-// });
-// port.pipe(parser)
-// var portATS = new SerialPort('',{
-//   baudRate: 57600
-// });
-// portATS.pipe(parserATS);
-=======
-var port = new SerialPort('COM3', {
-  autoOpen: false,
-  baudRate: 38400 
+var port = new SerialPort('/dev/cu.usbmodem14101', {
+  baudRate: 57600 
 });
 port.pipe(parser)
 var portATS = new SerialPort('COM5',{
@@ -83,7 +66,6 @@ var portATS = new SerialPort('COM5',{
   baudRate: 57600
 });
 portATS.pipe(parserATS);
->>>>>>> 71a5617b9f20b71c920dae60e5a449a352e27f8e
 
 var receivedData;
 var cleanData;
@@ -115,87 +97,6 @@ const csvWriter = createCsvWriter({
 //   } while (currentDate - date < milliseconds);
 // }
 
-<<<<<<< HEAD
-// port.open();
-// // port.on('open', function() {});
-
-// parser.on('data', function(data) {
-//   //setInterval(function(){
-//   receivedData = data.toString();
-//   var cleanData = receivedData.substring(receivedData.indexOf('\r\n')).replace(/(\r\n|\n|\r)/gm,"");
-//   k = cleanData.split(" ");
-//   var dataCSV = []
-
-//   var ID_Peserta = k[0];
-//   var waktu = k[1];
-//   var altitude = k[2]; 
-//   var temp = k[3];
-//   var humid = k[4];
-//   var pressure = k[5];
-//   var wind_dir = k[6];
-//   var wind_speed = k[7];
-//   var lintang = k[8];
-//   var bujur = k[9];
-
-//   // console.log(cleanData);
-//   console.log("------------------------");
-//   io.emit('arduino:data', {
-//     temps: temp,
-//     humids: humid,
-//     altitudes: altitude,
-//     pressures: pressure,
-//     windd: wind_dir,
-//     winds: wind_speed,
-//     lintangs: lintang,
-//     bujurs: bujur
-//   });
-
-  // portATS.emit('arduino:data1',{
-  //   lintangs: lintang,
-  //   bujurs: bujur,
-  //   altitudes: altitude
-  // });
-
-  // dataCSV.push({
-  //   idP: k[0],
-  //   waktu: k[1],
-  //   ketinggian: k[2],
-  //   temperature: k[3],
-  //   kelembapan: k[4],
-  //   tekanan: k[5],
-  //   arah_angin: k[6],
-  //   kec_angin: k[7],
-  //   lintang: k[8],
-  //   bujur: k[9]
-  // })
-  // csvWriter.writeRecords(dataCSV).then(()=> console.log('CSV written'));
-  //port.close()
-// });
-
-//var j = 1;
-//   portATS.on('arduino:data1', function(data){
-//     //autotrack
-//     const autotrack = require('./Geo_calculator.js')
-//     //console.log(autotrack.data.calculate_compass_bearing(100,45,10,0))
-//     bearing = autotrack.data.calculate_compass_bearing(data.lintangs,data.bujurs,origin_latitude,origin_longitude);
-//     vertical = autotrack.data.calculate_vertical_angle(autotrack.data.calculate_distance(data.lintangs,data.bujurs,origin_latitude,origin_longitude),data.altitudes);
-//     Hasil_Autotrack = (Math.round(bearing)).toString() + " " + (Math.round(vertical)).toString();
-//     portATS.write(Hasil_Autotrack + "\n" function (err) {
-//       if (err) {
-//         return console.log('Error on write: ', err.message)
-//       }
-//       console.log(Hasil_Autotrack)
-//     })
-
-//     // if(j % 5 == 0){
-//     //   portATS.flush(function(err,results){});
-//     //   //portATS.disconnect()
-//     // }
-//     // j++;
-    
-//     //
-// })
-=======
 port.on('open', function() {});
 portATS.on('open', function() {});
 
@@ -276,5 +177,4 @@ parser.on('data', function(data) {
       console.log(Hasil_Autotrack)
     })
 })
->>>>>>> 71a5617b9f20b71c920dae60e5a449a352e27f8e
 server.listen(3000)
